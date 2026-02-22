5d0cb0554d069c34af9d39837cc118bb1e8aa564
Rida Abdallah <rida.abdallah@solteo.fr>
2026-02-22 14:47:06 +0200

    fix: vercel issue 4

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,4 +1,4 @@
-export { default as CheckoutBookForm, type CheckoutBookFormProps } from "./checkoutForm/checkoutForm";
+export { default as CheckoutBookForm, type CheckoutBookFormProps } from "./checkoutBookForm/checkoutBookForm";
 export { default as BookForm, type BookFormProps } from "./bookform/bookform";
 export { formClasses, type FormClasses } from "./bookform/bookform.styling";
 export { default as Header } from "./header/header";
